ac089f624a18302dddb7889758e6a40b6f3d69d9
A7Coder <a7coder@gmail.com>
2023-05-02 14:25:33 +0530

    All images are now png

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["drive.google.com"],
-    formats: ['image/avif', 'image/webp','image/jpg','image/png','image/gif'],
+    
  },
  i18n: {
   locales: ["en"],
