2c2ead229b4419c1f32d4715663060b4116fcffc
A7Coder <a7coder@gmail.com>
2023-05-02 14:40:42 +0530

    Used html img tag in card

--- components/card.js
+++ components/card.js
@@ -1,5 +1,5 @@
 import styles from "../styles/card.module.css";
-import Image from "next/image";
+// import Image from "next/image";
 import Link from "next/link";
 export default function Card({children,title,img_url,desc_text,repo_url}) {
   return (
@@ -7,7 +7,7 @@ export default function Card({children,title,img_url,desc_text,repo_url}) {
    
       <div className={styles.card} >
       <Link href={repo_url} className={styles.card_link}>
-        <Image src={img_url} width={200} height={300} alt={title} title={title} priority={true}/>
+        <img src={img_url} width={200} height={300} alt={title}  />
         <div className={styles.text}>
         <h2>{title}</h2>
         <p>{desc_text}</p>
